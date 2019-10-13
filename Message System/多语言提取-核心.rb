3a8d533f6cd9b5d0d5fbadb0e607d2554ffe6882
Eagle <eagle_zhou@outlook.com>
2019-10-13 12:04:54 +0800

    一系列清理

--- Message System/多语言提取-核心.rb
+++ Message System/多语言提取-核心.rb
@@ -1,8 +1,23 @@
-# TODO
+#==============================================================================
+# ■ 多语言提取-核心 by 老鹰（http://oneeyedeagle.lofter.com/）
+#==============================================================================
+$imported ||= {}
+$imported["EAGLE-MultiLangCore"] = true
+#=============================================================================
+# - 2019.10.13.11
+#=============================================================================
+# - 本插件提供了从字符串中提取指定语言的标签对内容的方法
+# - 利用 <sym></sym> 标签对编写不同语言的内容
+#   其中 sym 替换成所设置的语言符号的字符串
+# - 示例：
+#     t = "<cn>...</cn><en>...</en>"
+#     t2 = MultiLang.extract(t)
+#     若当前语言为 :CN，则 t2 = "<cn>...</cn>"
+#-----------------------------------------------------------------------------
 
 module MultiLang
   #--------------------------------------------------------------------------
-  # ● 常量定义
+  # ● 常量
   #--------------------------------------------------------------------------
   # 全部语言符号的数组
   # （推荐符号全大写，但在文本中标签大小写均可）
@@ -15,16 +30,16 @@ module MultiLang
   #--------------------------------------------------------------------------
   # ● 设置当前语言
   #--------------------------------------------------------------------------
-  def self.set(symbol = DEFAULT_LANGUAGE)
-    return if !ALL_LANGUAGES.include?(symbol)
-    $game_system.eagle_lang = symbol
+  def self.set(sym = DEFAULT_LANGUAGE)
+    return if !ALL_LANGUAGES.include?(sym)
+    $game_system.eagle_lang = sym
   end
   #--------------------------------------------------------------------------
   # ● 提取当前语言的文本
   #--------------------------------------------------------------------------
   def self.extract(text)
     sym_def = NO_LABEL_TEXT_TYPE
-    sym_cur = $game_system.symbol_LANGUAGE
+    sym_cur = $game_system.eagle_lang
     ALL_LANGUAGES.each do |l|
       # 无标签文本在全部语言下显示 or 无标签文本归类到 当前语言 下
       if sym_def == :ALL || sym_def == sym_cur
@@ -37,7 +52,8 @@ module MultiLang
     end
     return text
   end
-end # end of module MultiLANGUAGE
+end # end of module MultiLang
+
 class Game_System
   attr_accessor :eagle_lang
   #--------------------------------------------------------------------------
