967d4c57237a4ae1a9f6092690e3966abbf00c5e
Eagle <eagle_zhou@outlook.com>
2020-02-08 13:49:19 +0800

    事件指令跳过 新增提示文本绘制

--- Event System/事件指令跳过.rb
+++ Event System/事件指令跳过.rb
@@ -1,17 +1,17 @@
 #==============================================================================
 # ■ 事件指令跳过 by 老鹰（http://oneeyedeagle.lofter.com/）
 #==============================================================================
-# - 2020.2.7.18
+# - 2020.2.8.11 新增提示文本
 #==============================================================================
 # - 本插件利用标签，新增了事件指令的跳过
 #--------------------------------------------------------------------------
-# - 在开始跳过判定的指令前新增标签，内容包含 SKIP ，
+# - 在开始跳过判定的指令前新增标签，内容为 SKIP ，
 #   在结束跳过判定的指令后新增标签，内容为 SKIPEND ，
 #   则在执行 SKIP 与 SKIPEND 之间的指令时，按下按键即可快速跳过中间的指令。
 #
 # - 示例：
 #    当前地图的 1 号事件的第 1 页中指令列表
-#    |- 标签：SKIP 测试用1
+#    |- 标签：SKIP
 #    |- 显示文字：测试语句1
 #    |- 显示心情：惊讶
 #    |- 显示文字：测试语句2
@@ -28,7 +28,7 @@
 #
 # - 示例：
 #    当前地图的 1 号事件的第 2 页中指令列表
-#    |- 标签：SKIP 测试用
+#    |- 标签：SKIP
 #    |- 显示文字：测试语句1
 #    |- 显示文字：测试语句2
 #    |- 标签：SKIPELSE
@@ -45,9 +45,9 @@
 
 module COMMAND_SKIP
   #--------------------------------------------------------------------------
-  # ● 【常量】跳过开始的标签所含内容（正则匹配）
+  # ● 【常量】跳过开始的标签内容（完全匹配）
   #--------------------------------------------------------------------------
-  LABEL_SKIP_REGEXP = /SKIP/i
+  LABEL_SKIP = "SKIP"
   #--------------------------------------------------------------------------
   # ● 【常量】跳过时需要执行的标签内容（完全匹配）
   #--------------------------------------------------------------------------
@@ -62,6 +62,49 @@ module COMMAND_SKIP
   def self.trigger?
     Input.trigger?(:A)
   end
+  #--------------------------------------------------------------------------
+  # ● 设置提示文本精灵
+  #--------------------------------------------------------------------------
+  def self.set_skip_hint(sprite)
+    text = "  ○ Shift键 - 跳过"
+    sprite.bitmap = Bitmap.new(Graphics.width/2, 24)
+    sprite.bitmap.gradient_fill_rect(sprite.bitmap.rect,
+      Color.new(0,0,0,150), Color.new(0,0,0,0))
+    sprite.bitmap.draw_text(0,1,sprite.width,sprite.height,text, 0)
+
+    sprite.y = 50
+  end
+  #--------------------------------------------------------------------------
+  # ● 精灵操作
+  #--------------------------------------------------------------------------
+  class << self
+    attr_reader :sprite_hint
+  end
+  def self.init_hint
+    if @hint_show_count.nil?
+      @sprite_hint ||= Sprite.new
+      set_skip_hint(@sprite_hint)
+      @sprite_hint.visible = false
+    end
+  end
+  def self.show_hint
+    @sprite_hint.visible = true
+  end
+  def self.hide_hint
+    @sprite_hint.visible = false
+  end
+end
+#=============================================================================
+# ○ Game_CharacterBase
+#=============================================================================
+class Game_CharacterBase
+  #--------------------------------------------------------------------------
+  # ● 暂停移动
+  #--------------------------------------------------------------------------
+  def stop_move
+    @x = @real_x
+    @y = @real_y
+  end
 end
 #=============================================================================
 # ○ Game_Interpreter
@@ -72,6 +115,7 @@ class Game_Interpreter
   #--------------------------------------------------------------------------
   alias eagle_skip_run run
   def run
+    COMMAND_SKIP.init_hint
     @eagle_skip_begin_i = nil # 记录skip开始标签的指令位置
     eagle_skip_run
   end
@@ -88,12 +132,15 @@ class Game_Interpreter
   #--------------------------------------------------------------------------
   alias eagle_skip_command_118 command_118
   def command_118
-    if @params[0] =~ COMMAND_SKIP::LABEL_SKIP_REGEXP
+    if @params[0] == COMMAND_SKIP::LABEL_SKIP
+      COMMAND_SKIP.show_hint
       @eagle_skip_begin_i = @index
-    end
-    if @eagle_skip_begin_i && @params[0] == COMMAND_SKIP::LABEL_THEN
+    elsif @eagle_skip_begin_i && @params[0] == COMMAND_SKIP::LABEL_THEN
       eagle_goto_label(COMMAND_SKIP::LABEL_END)
+      COMMAND_SKIP.hide_hint
       @eagle_skip_begin_i = nil
+    elsif @params[0] == COMMAND_SKIP::LABEL_END
+      COMMAND_SKIP.hide_hint
     end
     eagle_skip_command_118
   end
@@ -115,13 +162,15 @@ class Game_Interpreter
   # ● 处理跳过
   #--------------------------------------------------------------------------
   def eagle_process_skip
-    @eagle_skip_begin_i = nil
     if $imported["EAGLE-MessageEX"]
       SceneManager.scene.message_window.force_close
     end
-    if eagle_goto_label(COMMAND_SKIP::LABEL_THEN)
-    else
+    $game_player.stop_move
+    $game_map.events.each { |id,c| c.stop_move }
+    if !eagle_goto_label(COMMAND_SKIP::LABEL_THEN)
       eagle_goto_label(COMMAND_SKIP::LABEL_END)
     end
+    COMMAND_SKIP.hide_hint
+    @eagle_skip_begin_i = nil
   end
 end
