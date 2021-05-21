3bda09ff2c90d081b43d020b146dba27fb3bd0ea
Eagle <eagle_zhou@outlook.com>
2021-05-21 18:39:20 +0800

    update

---
+++ TEMP/TEST.rb
@@ -0,0 +1,25 @@
+# 文本替换规则
+#
+#  替换优先级
+module EAGLE
+  def self.message_ex_replace(text)
+    text
+    text
+  end
+end
+#=============================================================================
+# ○ Game_Message
+#=============================================================================
+class Game_Message
+end
+class Window_EagleMessage
+  #--------------------------------------------------------------------------
+  # ● 替换转义符（此时依旧是 \\ 开头的转义符）
+  #--------------------------------------------------------------------------
+  alias eagle_text_replace_process_conv eagle_process_conv
+  def eagle_process_conv(text)
+    text = eagle_text_replace_process_conv(text)
+
+    text
+  end
+end
