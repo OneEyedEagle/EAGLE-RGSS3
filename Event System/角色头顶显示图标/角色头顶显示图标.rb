051eb1c086ae5dd6bdbd723fdad40b37c84d101a
Eagle <763375053@qq.com>
2022-04-06 22:34:11 +0800

    新增 事件调查提示

--- Event System/角色头顶显示图标/角色头顶显示图标.rb
+++ Event System/角色头顶显示图标/角色头顶显示图标.rb
@@ -1,19 +1,44 @@
 #==============================================================================
 # ■ 角色头顶显示图标 by 老鹰（http://oneeyedeagle.lofter.com/）
 #==============================================================================
-# - 2020.10.23.0 新增移动类型自定义
+$imported ||= {}
+$imported["EAGLE-EventPopIcon"] = "1.1.0"
+#==============================================================================
+# - 2022.4.6.20 参数扩展
 #==============================================================================
 # - 本插件新增了在地图上角色头顶显示指定图标的功能（仿气泡）
 #--------------------------------------------------------------------------
 # - 在 RGSS3 中，可以给 Game_CharacterBase类的 @balloon_id 变量赋值，
 #   用于在地图角色的头顶显示动态气泡
+#
 # - 类似的，本插件为 Game_Character类新增了 @pop_icon 变量，
 #     为其赋值能在一段时间内，在角色头顶显示图标
-#   同时，也新增了 @pop_icon_type 变量，用来指定不同的运动类型
+#
+# - 同时，也新增了 @pop_icon_params 的Hash类型变量，用来存储参数
+#
+#   有效参数一览：
+#
+#     :pos => 数字 → 数字为 0 时（默认值），图标显示在行走图上方
+#                     数字为 1 时，图标显示在行走图下方
+#                     数字为 2 时，图标显示在行走图中央
+#
+#     :dir => 数字 → 数字为 2 时，图标先下移，再上移
+#                     数字为 4 时，图标先左移，再右移
+#                     数字为 6 时，图标先右移，再左移
+#                     数字为 8 时（默认值），图标先上移，再下移
+#
+#     :opa => 数字 → 数字为 0 时（默认值），关闭显隐特效；为 1 时，开启显隐
+#
+#     :dx => 数字, :dy => 数字  → 坐标的增加偏移值
+#
 # - 示例：
 #    $game_player.pop_icon = 1
 #      → 在玩家头顶显示 1 号图标，持续 MAX_POP_FRAME 帧
-#   若设置为 0，则消除图标
+#    $game_player.pop_icon_params[:pos] = 1
+#      → 将显示的图标位置修改为玩家下方
+#
+# - 若 @pop_icon 设置为 0，则会立即消除图标
+#
 #==============================================================================
 module POP_ICON
   #--------------------------------------------------------------------------
@@ -22,48 +47,97 @@ module POP_ICON
   S_ID_NO_POP = 1
   #--------------------------------------------------------------------------
   # ● 【常量】一次激活后，最长的显示帧数
-  #  若设置为 nil，则不会自动消失
+  #  若设置为 nil，则图标不会自动消失
   #--------------------------------------------------------------------------
   MAX_SHOW_FRAME = 10
   #--------------------------------------------------------------------------
-  # ● 【常量】最大循环帧数
+  # ● 【常量】在 draw_pop_icon 方法中的最大循环帧数
+  #  用于设置完整的移动流程，可以大于 MAX_SHOW_FRAME，来制作更完备的动态效果
   #--------------------------------------------------------------------------
   MAX_LOOP_FRAME = 60
   #--------------------------------------------------------------------------
-  # ● 按照当前循环的帧序号进行重绘
+  # ● 按照当前循环的帧序号进行重绘（每帧调用该方法）
   #  frame 的取值为 0 ~ MAX_LOOP_FRAME-1
   #--------------------------------------------------------------------------
-  def self.draw_pop_icon(sprite_chara, sprite_icon, icon_id, frame, move_type)
-    # 设置基础位置
-    sprite_icon.x = sprite_chara.x
-    sprite_icon.y = sprite_chara.y - sprite_chara.height
-    sprite_icon.z = sprite_chara.z + 200
-
-    # 在第一帧时，新建位图并绘制图标
+  def self.draw_pop_icon(sprite_chara, sprite_icon, icon_id, frame, ps)
+    # 在第一帧，新建位图并绘制图标
     if frame == 0
       sprite_icon.visible = true
       sprite_icon.bitmap ||= Bitmap.new(24, 24)
       sprite_icon.bitmap.clear
-      sprite_icon.ox = 12
-      sprite_icon.oy = 24
       draw_icon(sprite_icon.bitmap, icon_id, 0, 0)
       sprite_icon.opacity = 255
+
+      # 依据放置位置，设置原点
+      case ps[:pos]
+      when 0 # （默认）放置于事件头顶，显示原点为图标的顶部中点
+        sprite_icon.ox = sprite_icon.width / 2
+        sprite_icon.oy = sprite_icon.height
+      when 1 # 放置于事件脚底，显示原点为图标的底部中点
+        sprite_icon.ox = sprite_icon.width / 2
+        sprite_icon.oy = 0
+      when 2 # 放置于事件中心
+        sprite_icon.ox = sprite_icon.width / 2
+        sprite_icon.oy = sprite_icon.height / 2
+      end
     end
 
-    if move_type == 0
-    elsif move_type == 1
-      sprite_icon.y = sprite_chara.y - sprite_chara.height / 2
+    # 定下基础位置，之后根据帧数计算出xy的偏移值
+    sprite_icon.x = sprite_chara.x
+    sprite_icon.y = sprite_chara.y - sprite_chara.height
+    sprite_icon.z = sprite_chara.z + 200
+    # 变更基础位置
+    case ps[:pos]
+    when 0
+    when 1
+      sprite_icon.y = sprite_chara.y
+    when 2
+      sprite_icon.y = sprite_chara.y - sprite_icon.height / 2
     end
 
-    case frame
-    when 1..29
-      sprite_icon.opacity -= 6
-      sprite_icon.y += (frame/4)
-    when 30..59
-      sprite_icon.opacity += 6
-      sprite_icon.y += ((29-(frame-29))/4)
+    case ps[:dir]
+    when 2 # 先下移，再上移
+      case frame
+      when 1..29
+        sprite_icon.y += (frame/4)
+      when 30..59
+        sprite_icon.y += ((29-(frame-29))/4)
+      end
+    when 4 # 先左移，再右移
+      case frame
+      when 1..29
+        sprite_icon.x -= (frame/4)
+      when 30..59
+        sprite_icon.x -= ((29-(frame-29))/4)
+      end
+    when 6 # 先右移，再左移
+      case frame
+      when 1..29
+        sprite_icon.x += (frame/4)
+      when 30..59
+        sprite_icon.x += ((29-(frame-29))/4)
+      end
+    when 8 # 先上移，再下移
+      case frame
+      when 1..29
+        sprite_icon.y -= (frame/4)
+      when 30..59
+        sprite_icon.y -= ((29-(frame-29))/4)
+      end
     end
 
+    if ps[:opa].to_i == 1
+      case frame  # 显隐切换
+      when 1..29
+        sprite_icon.opacity -= 6
+      when 30..59
+        sprite_icon.opacity += 6
+      end
+    end
+
+    # 最后，再增加偏移
+    sprite_icon.x += ps[:dx].to_i if ps[:dx]
+    sprite_icon.y += ps[:dy].to_i if ps[:dy]
   end
   #--------------------------------------------------------------------------
   # ● 绘制图标
@@ -79,15 +153,23 @@ end
 # ○ Game_Character
 #=============================================================================
 class Game_Character
-  attr_accessor :pop_icon, :pop_icon_type
+  attr_accessor :pop_icon, :pop_icon_params
   #--------------------------------------------------------------------------
   # ● 初始化公有成员变量
   #--------------------------------------------------------------------------
   alias eagle_popicon_init initialize
   def initialize
     @pop_icon = 0
-    @pop_icon_type = 0
+    @pop_icon_params = { :pos => 0, :dx => 0, :dy => 0, :dir => 8, :opa => 0 }
     eagle_popicon_init
+  end
+  #--------------------------------------------------------------------------
+  # ● 处理参数，确保参数存在，类型正确
+  #--------------------------------------------------------------------------
+  def get_popicon_params
+    @pop_icon_params[:pos] = @pop_icon_params[:pos].to_i
+    @pop_icon_params[:dir] = @pop_icon_params[:dir].to_i
+    return @pop_icon_params
   end
 end
 #=============================================================================
@@ -122,11 +204,11 @@ class Sprite_Character < Sprite_Base
       return
     end
     reset_popicon if @popicon_sprite.nil?
-    flag_continue_show = false
     if @character.pop_icon > 0
       if @character.pop_icon == @pop_icon # 若与当前显示一致，则继续显示
+        @popicon_count = @popicon_count % POP_ICON::MAX_LOOP_FRAME
+        @popicon_last_activate = @popicon_count
         @character.pop_icon = -1
-        flag_continue_show = true
       else
         reset_popicon
       end
@@ -134,13 +216,12 @@ class Sprite_Character < Sprite_Base
       return end_popicon if @character.pop_icon == 0
     end
     if @pop_icon > 0
-      if !flag_continue_show && POP_ICON::MAX_SHOW_FRAME &&
-         @popicon_count > POP_ICON::MAX_SHOW_FRAME
+      if @popicon_count - @popicon_last_activate > POP_ICON::MAX_SHOW_FRAME
         return end_popicon
       end
       c = @popicon_count % POP_ICON::MAX_LOOP_FRAME
       POP_ICON.draw_pop_icon(self, @popicon_sprite, @pop_icon, c,
-        @character.pop_icon_type)
+        @character.get_popicon_params)
       @popicon_sprite.update
       @popicon_count += 1
     end
@@ -153,6 +234,7 @@ class Sprite_Character < Sprite_Base
     @character.pop_icon = -1 # 在显示中，置为-1
     @popicon_sprite ||= Sprite.new(viewport)
     @popicon_count = 0
+    @popicon_last_activate = 0  # 上一次激活icon时的计数帧
   end
   #--------------------------------------------------------------------------
   # ● 释放图标pop
